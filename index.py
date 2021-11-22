3bbc73444a04a580f8e1c8bfc501542236eb6d56
Steven Weatherspoon <stvnwthrsp@gmail.com>
2021-11-22 18:14:56 -0500

    Added missing parentheses

--- index.py
+++ index.py
@@ -187,7 +187,7 @@ class CategoryIntentHandler(AbstractRequestHandler):
         session_attributes["game_questions"] = getGameQuestions(category_id, NUMBER_OF_QUESTIONS*len(session_attributes["players"]))
         session_attributes["current_question_index"] = 0
 
-        speech_text = f"Okay, we'll play {category_string} trivia. Let's begin the game! Question {1}. {session_attrributes['game_questions'][0]["question"]} "
+        speech_text = f"Okay, we'll play {category_string} trivia. Let's begin the game! Question {1}. {session_attrributes['game_questions'][0]['question']} "
         reprompt_text = f"{session_attrributes['game_questions'][0]["question"]} "
 
         possible_answers = session_attrributes['game_questions'][0]["incorrect_answers"].append(session_attrributes['game_questions'][0]["correct_answer"])
